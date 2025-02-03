fe85550b26b413eb16ab59e4a0a8451277be34f9
David Kimmel <kimmel.david@gmail.com>
2025-02-03 17:52:27 -0500

    1-1

--- js/geojsonTutorial.js
+++ js/geojsonTutorial.js
@@ -1,19 +1,22 @@
+// Define a GeoJSON feature representing a point location (Coors Field)
 var geojsonFeature = {
     "type": "Feature",
     "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
+        "name": "Coors Field", // Name of the feature
+        "amenity": "Baseball Stadium", // Type of location
+        "popupContent": "This is where the Rockies play!" // Text for the popup
     },
     "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
+        "type": "Point", // Specifies that this is a point feature
+        "coordinates": [-104.99404, 39.75621] // Longitude and latitude of the point
     }
 };
+
+// Add the GeoJSON feature to the map
 L.geoJSON(geojsonFeature).addTo(map);
 
 
-
+// Define a set of LineString features (lines connecting multiple coordinates)
 var myLines = [{
     "type": "LineString",
     "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
@@ -22,27 +25,31 @@ var myLines = [{
     "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
 }];
 
+// Define a style object to customize the appearance of the lines
 var myStyle = {
-    "color": "#ff7800",
-    "weight": 5,
-    "opacity": 0.65
+    "color": "#ff7800", // Orange color
+    "weight": 5, // Line thickness
+    "opacity": 0.65 // Transparency level
 };
 
+// Add the LineString features to the map and apply the custom style
 L.geoJSON(myLines, {
     style: myStyle
 }).addTo(map);
 
+
+// Define two polygon features representing different political party regions
 var states = [{
     "type": "Feature",
-    "properties": {"party": "Republican"},
+    "properties": {"party": "Republican"}, // Assigns a property for styling
     "geometry": {
-        "type": "Polygon",
+        "type": "Polygon", // Defines a polygon shape
         "coordinates": [[
             [-104.05, 48.99],
             [-97.22,  48.98],
             [-96.58,  45.94],
             [-104.03, 45.94],
-            [-104.05, 48.99]
+            [-104.05, 48.99] // Defines the boundary by connecting points
         ]]
     }
 }, {
@@ -60,22 +67,26 @@ var states = [{
     }
 }];
 
+// Add the polygon features to the map with conditional styling based on properties
 L.geoJSON(states, {
     style: function(feature) {
         switch (feature.properties.party) {
-            case 'Republican': return {color: "#ff0000"};
-            case 'Democrat':   return {color: "#0000ff"};
+            case 'Republican': return {color: "#ff0000"}; // Red for Republican
+            case 'Democrat':   return {color: "#0000ff"}; // Blue for Democrat
         }
     }
 }).addTo(map);
 
+
+// Define a function to bind popups to features dynamically
 function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
+    // Check if the feature has a popupContent property
     if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
+        layer.bindPopup(feature.properties.popupContent); // Bind popup text
     }
 }
 
+// Define another GeoJSON feature with a popup
 var geojsonFeature = {
     "type": "Feature",
     "properties": {
@@ -89,7 +100,41 @@ var geojsonFeature = {
     }
 };
 
+// Add the feature to the map with the custom popup binding function
 L.geoJSON(geojsonFeature, {
     onEachFeature: onEachFeature
 }).addTo(map);
+
+
+/*
+Define multiple point features with a filtering property
+var someFeatures = [{
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "show_on_map": true // Determines if the feature should be displayed
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+}, {
+    "type": "Feature",
+    "properties": {
+        "name": "Busch Field",
+        "show_on_map": false // This feature will be filtered out
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.98404, 39.74621]
+    }
+}];
+
+// Add the features to the map, filtering out those where show_on_map is false
+L.geoJSON(someFeatures, {
+    filter: function(feature, layer) {
+        return feature.properties.show_on_map;
+    }
+}).addTo(map);
+*/
 
--- js/quickstartTutorial.js
+++ js/quickstartTutorial.js
@@ -1,43 +1,54 @@
-
+// Initialize the map and set its view to the coordinates and zoom level
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-//add tile layer...replace project id and accessToken with your own
+// Add a tile layer (the base map) using OpenStreetMap tiles
+// The URL structure defines how the tiles are loaded dynamically based on zoom and location
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    maxZoom: 19, // Sets the maximum zoom level allowed on the map
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' // Adds attribution text
+}).addTo(map); // Adds the tile layer to the map
 
+// Add a marker to the map at the specified coordinates
 var marker = L.marker([51.5, -0.09]).addTo(map);
 
+// Add a circle overlay to the map at the specified coordinates
 var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
+    color: 'red', // Sets the border color of the circle
+    fillColor: '#f03', // Sets the fill color inside the circle
+    fillOpacity: 0.5, // Controls the opacity of the fill color
+    radius: 500 // Defines the radius of the circle in meters
 }).addTo(map);
 
+// Add a polygon (a shape with multiple points) to the map
 var polygon = L.polygon([
     [51.509, -0.08],
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(map);
 
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+// Bind a popup to the marker that appears when the marker is clicked
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup(); // Opens the popup immediately
+
+// Bind popups to the circle and polygon
 circle.bindPopup("I am a circle.");
 polygon.bindPopup("I am a polygon.");
 
+// Create a standalone popup and display it on the map at the given coordinates
 var popup = L.popup()
-    .setLatLng([51.513, -0.09])
-    .setContent("I am a standalone popup.")
-    .openOn(map);;
+    .setLatLng([51.513, -0.09]) // Sets the popup’s latitude and longitude
+    .setContent("I am a standalone popup.") // Defines the text content of the popup
+    .openOn(map); // Adds the popup to the map and closes any other open popups
 
+// Create an empty popup object
 var popup = L.popup();
 
+// Define a function that displays a popup showing the coordinates where the user clicked on the map
 function onMapClick(e) {
     popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
+        .setLatLng(e.latlng) // Sets the popup location to the clicked position
+        .setContent("You clicked the map at " + e.latlng.toString()) // Sets popup content with the clicked coordinates
+        .openOn(map); // Adds the popup to the map
 }
 
+// Attach an event listener to the map that triggers the onMapClick function when the map is clicked
 map.on('click', onMapClick);
